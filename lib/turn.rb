3d3701f3b2c7b8db9bfcdf1650e7f6b5e3f18568
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-12-01 04:53:23 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,12 +39,8 @@ user_input = gets.strip
 index = input_to_index(user_input)
 if valid_move?(board, index)
   move(board, index, character = "X")
-  puts "#{index + 1}"
+  puts "#{index}"
+else turn(board)
+end
   display_board(board)
-else
-  until valid_move?(board, index)
-puts "Please enter 1-9:"
-index = input_to_index(user_input)
-end
-end
 end
