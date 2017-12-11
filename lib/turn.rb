1fa474fc598601d8a6918fc5010131fc231d3ba6
Matthew McDonald <matthew.mcd1031@gmail.com>
2017-12-11 03:22:16 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
@@ -11,23 +10,18 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
-  true
-end
-end
-
-
 def position_taken?(board, index)
-  if (board[index] == "" || board[index] == " " || board[index] == nil) == true
-    taken = false
-  else
-    taken = true
+  board[index] != " "
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
   end
 end
 
-def move(board, user_input, character = "X")
-  board[user_input] = character
+def move(board, index, character = "X")
+  board[index] = character
 end
 
 def turn(board)
@@ -35,9 +29,9 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-move(board, index)
-display_board(board)
+    move(board, index)
+    display_board(board)
   else
-  turn(board)
-end
+    turn(board)
+  end
 end
