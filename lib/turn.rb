e776b48df9b7a7f9cbfcf0f03a0290fbf8347a20
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-25 00:51:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,7 @@ puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+user_input.to_i - 1
+end
